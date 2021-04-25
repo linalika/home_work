33f5aaeb03beecb793eaed898f6ef5fac5b29e99
alina stepanova <gklina9@yandex.ru>
2021-04-25 11:41:13 +0300

    add task_6

---
+++ task_6.py
@@ -0,0 +1,8 @@
+day_result = float(input("Введите количество начальное количество: "))
+goal_result = float(input("Введите желаемое количество километров: "))
+days = 1
+while goal_result > day_result:
+    day_result += day_result / 10
+    days += 1
+
+print(f"Вы достигните результата на {days} день")
