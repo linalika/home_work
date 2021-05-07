1f334995c34cdbf7353882f7ae81df5a3f487d6d
alina stepanova <gklina9@yandex.ru>
2021-05-07 09:24:23 +0300

    add task_2

---
+++ task_2.py
@@ -0,0 +1,13 @@
+user_list = list(input('Введите список элементов: ').split())
+print(user_list)
+count = 1
+for i in range(0, len(user_list), 2):
+    if count == len(user_list):
+        break
+    elif i + 3 == len(user_list):
+        break
+    else:
+        temporary = user_list[i]
+        user_list[i] = user_list[i + 1]
+        user_list[i + 1] = temporary
+print(user_list)
