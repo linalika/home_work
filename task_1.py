81d226ffa47a86670284afe3e3eaafe30caa4689
alina stepanova <gklina9@yandex.ru>
2021-05-11 19:54:32 +0300

    add task_1

---
+++ task_1.py
@@ -0,0 +1,24 @@
+# Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
+# Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+
+
+def div(dividend, divider):
+    if dividend.isdigit() and divider.isdigit():
+        dividend = float(dividend)
+        divider = float(divider)
+        try:
+            result = round(dividend / divider, 2)
+        except ArithmeticError:
+            result = 'Ошибка, деление на ноль!'
+    else:
+        result = 'Вы ввели не число'
+    return result
+
+
+def main():
+    number_1 = input('Введите делимое: ')
+    number_2 = input('Введите делитель: ')
+    print(div(number_1, number_2))
+
+
+main()
