8293e94157f5911952e408f84e4279b1fabd364f
alina stepanova <gklina9@yandex.ru>
2021-05-13 21:20:47 +0300

    add correction

--- task_6_7.py
+++ task_6_7.py
@@ -1,12 +1,19 @@
 # Решение для 6 и 7 заданий
 
-def int_func(user_string):
-    return user_string.title()
+def int_func(*args):
+    user_string = list(args)
+    latin_char = 'qwertyuiopasdfghjklzxcvbnm'
+    for i in range(len(user_string)):
+        if not set(user_string[i]).difference(latin_char):
+            user_string[i] = user_string[i].title()
+        else:
+            user_string.remove(user_string[i])
+    return user_string
 
 
 def main():
-    user_string = input('Введите слова: ')
-    result = int_func(user_string)
+    user_string = input('Введите слова: ').split()
+    result = int_func(*user_string)
     print(result)
 
 
