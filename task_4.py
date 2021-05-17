621ceb9c4d8daa3a44d0f1d0e715aa36c2317e47
alina stepanova <gklina9@yandex.ru>
2021-05-17 20:15:20 +0300

    add correction

--- task_4.py
+++ task_4.py
@@ -1,4 +1,6 @@
-my_list = [99, 45, 2, 2, 11, 55, 11, 67, 1, 8, 1, 2, 99, 30]
+from random import randint
+
+my_list = [randint(-10, 10) for _ in range(10)]
 
 my_new_list = [el for el in my_list if my_list.count(el) == 1]
 print(f'Исходный список {my_list}')
